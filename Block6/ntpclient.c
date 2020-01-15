8dfdbae9ddb43fccd59120104e9f33261f327ea5
hungbui7698 <hungbui7698@DESKTOP-85VV9B5.localdomain>
2020-01-15 15:42:05 +0100

    everything is done with a calloc

--- Block6/ntpclient.c
+++ Block6/ntpclient.c
@@ -18,7 +18,8 @@ int main(int argc, char** argv) {
     int serverNumber = argc-2;
     double RTT_max;
     double RTT_min;
-    double RTT[requestNumber];
+    double *RTT = calloc(requestNumber,sizeof(double));
+     //double RTT[requestNumber];
 
     for (int i = 0; i < serverNumber; i++) {
         for (int j = 0; j < requestNumber; j++) {
@@ -86,15 +87,15 @@ int main(int argc, char** argv) {
             float rootDispersion;
             analizeTheResponse(ntpResponse,&T2_unix,&T3_unix,&rootDispersion);
 
-            printf("RTT[35]= %lf \n",RTT[35]);
-            printf("RTT[34]= %lf \n",RTT[34]);
-            printf("RTT[33]= %lf \n",RTT[33]);
-            printf("RTT[99]= %lf \n",RTT[99]);
-            printf("RTT[36]= %lf \n",RTT[36]);
+            //printf("RTT[2]= %lf \n",RTT[2]);
+            //printf("RTT[35]= %lf \n",RTT[35]);
+            //printf("RTT[99]= %lf \n",RTT[99]);
+
 
             RTT[j] = (T4_unix - T1_unix) -(T3_unix - T2_unix);
+
             //RTT[35] = RTT[34];
-            printf("RTT[%d] = %lf \n",j,RTT[j]);
+            //printf("RTT[%d] = %lf \n",j,RTT[j]);
             double delay = RTT[j]/2;
             double offset = (T2_unix - T1_unix + T3_unix -T4_unix)/2;
             double Dispersion_of_8_Anfragen;
@@ -104,11 +105,11 @@ int main(int argc, char** argv) {
                 for (int a = j - 6; a <= j; a++) {
                     if (RTT[a] < RTT_min) {
                         RTT_min = RTT[a];
-                        printf ("min: %lf of RTT: %d \n" ,RTT_min,a);
+                        //printf ("min: %lf of RTT: %d \n" ,RTT_min,a);
                     }
                     if (RTT[a] > RTT_max) {
                         RTT_max = RTT[a];
-                        printf ("max: %lf of RTT: %d \n" ,RTT_max,a);
+                        //printf ("max: %lf of RTT: %d \n" ,RTT_max,a);
                     }
                 }
             } else {
@@ -128,6 +129,7 @@ int main(int argc, char** argv) {
             //if (offset <-100000) offset = 0;
             Dispersion_of_8_Anfragen = RTT_max - RTT_min;
             printf("%s;   %d;    %f;    %lf;    %lf;    %lf\n",argv[i+2],j+1,rootDispersion,Dispersion_of_8_Anfragen,delay,offset);
+            //if(j = 35) RTT[35] = RTT[j];
             //printf("%lf;    %lf;     %lf;      %lf\n",T1_unix,T2_unix,T3_unix,T4_unix);
             sleep(1);
         }
